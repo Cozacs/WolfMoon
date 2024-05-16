66465c82f53e53f88a74be6f4768d9c01407cd8d
Cozac <victorcozac32@gmail.com>
2024-05-16 05:56:29 -0300

    Structure Development

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import './Contact.css';
+import Header from '../../Components/Header/Header';
+
+export default function Contact () {
+    return (
+        <>
+        <Header />
+        Contact
+        </>
+    );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+import Header from '../../Components/Header/Header';
+
+export default function Home () {
+    return (
+        <>
+        <Header />
+        Home
+        </>
+    );
+}
