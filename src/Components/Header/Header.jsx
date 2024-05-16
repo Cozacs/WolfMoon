20a9c510d7b4e5f909caa3b97f3f3e6ba6aebba9
Cozac <victorcozac32@gmail.com>
2024-05-16 06:45:12 -0300

    Strucute Done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,12 @@ import './Header.css';
 export default function Header() {
     return (
         <header>
-            <img src="" alt="Logo" />
+            <div className='LogoContainer'>
+                <img src="assets/LogoWolf.png" alt="LogoWolf" />
+                <img src="assets/LogoBrand.png" alt="LogoBrand" />
+            </div>
             <nav>
-
+                
             </nav>
         </header>
     )
